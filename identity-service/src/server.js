f30cd76eebbab046487ffb762a3f2bb4fd4d9a94
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-17 22:15:15 +0600

    refactor: Updated identity-service/combined.log, src/server.js

--- identity-service/src/server.js
+++ identity-service/src/server.js
@@ -10,6 +10,8 @@ const logger = require("./utils/logger");
 
 const Redis = require("ioredis");
 const { RateLimiterRedis } = require("rate-limiter-flexible");
+const { default: rateLimit } = require("express-rate-limit");
+const { default: RedisStore } = require("rate-limit-redis");
 
 const app = express();
 
@@ -29,6 +31,9 @@ app.use((req, res, next) => {
   next();
 });
 
+// Rate limiter using Redis: limits each IP to 10 requests per second
+// Helps protect the server from abuse, DDoS, or request flooding
+
 const rateLimiter = new RateLimiterRedis({
   storeClient: redisClient,
   keyPrefix: "middleware",
@@ -41,13 +46,38 @@ app.use((req, res, next) => {
     .consume(req.ip)
     .then(() => next())
     .catch(() => {
-      logger.warn(`Rate limit exceeded for IP: ${req.qip}`);
+      logger.warn(`Rate limit exceeded for IP: ${req.ip}`);
       res.status(429).json({
         success: false,
         message: "Too many requests",
       });
     });
 });
+
+const sensitiveEndpointsLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 50,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res) => {
+    logger.warn(`Sensitive endpoint rate limit exceeded for IP: ${req.ip}`);
+    res.status(429).json({
+      success: false,
+      message: "Too many requests",
+    });
+  },
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
+});
+
+//apply this sensitiveEndpointsLimiter to our routes
+
+app.use("/api/auth/register", sensitiveEndpointsLimiter);
+
+// Routes
+
+app.use("/api/auth", route);
 
 mongoose
   .connect(process.env.MONGODB_URL)
