cf35d466200375f5873fa584d5a4c1ee7344f42b
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-17 20:51:15 +0600

    feat: Added .gitignore, identity-service/combined.log, identity-service/error.log | refactor: Updated src/server.js

--- identity-service/src/server.js
+++ identity-service/src/server.js
@@ -1,4 +1,15 @@
 //
-console.log(
-  `hellow world, from bangladesh. i love my wife more than everything in the worlld`,
-);
+
+require("dotenv").config();
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const logger = require("./utils/logger");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => logger.info("Connected to mongodb"))
+  .catch((e) => logger.error("Mongo connection error", e));
