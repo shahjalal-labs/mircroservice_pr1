02ea2017a98405313178e59b9168ed9b8cc031eb
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-17 21:12:15 +0600

    feat: Added rate-limiter-flexible/LICENSE.md, rate-limiter-flexible/README.md, rate-limiter-flexible/index.js, lib/BurstyRateLimiter.js, lib/ExpressBruteFlexible.js, lib/RLWrapperBlackAndWhite.js, lib/RateLimiterAbstract.js, lib/RateLimiterCluster.js, lib/RateLimiterDrizzle.js, lib/RateLimiterDrizzleNonAtomic.js, lib/RateLimiterDynamo.js, lib/RateLimiterEtcd.js, lib/RateLimiterEtcdNonAtomic.js, lib/RateLimiterMemcache.js, lib/RateLimiterMemory.js, lib/RateLimiterMongo.js, lib/RateLimiterMySQL.js, lib/RateLimiterPostgres.js, lib/RateLimiterPrisma.js, lib/RateLimiterQueue.js, lib/RateLimiterRedis.js, lib/RateLimiterRes.js, lib/RateLimiterSQLite.js, lib/RateLimiterStoreAbstract.js, lib/RateLimiterUnion.js, lib/RateLimiterValkey.js, lib/RateLimiterValkeyGlide.js, BlockedKeys/BlockedKeys.js, BlockedKeys/index.js, MemoryStorage/MemoryStorage.js, MemoryStorage/Record.js, MemoryStorage/index.js, component/RateLimiterEtcdTransactionFailedError.js, component/RateLimiterQueueError.js, component/RateLimiterSetupError.js, lib/constants.js, rate-limiter-flexible/package.json, rate-limiter-flexible/types.d.ts | refactor: Updated identity-service/combined.log, identity-service/package.json, src/server.js

--- identity-service/src/server.js
+++ identity-service/src/server.js
@@ -1,13 +1,28 @@
 //
 
 require("dotenv").config();
+
+const helmet = require("helmet");
 const express = require("express");
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const logger = require("./utils/logger");
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+//p: n! Secures the app by setting various HTTP headers
+//p: n! Helps prevent common attacks like XSS, clickjacking, and MIME sniffing
+app.use(helmet());
+app.use(express.json());
+app.use(cors());
+
+app.use((req, res, next) => {
+  logger.info(`Received ${req.method} request to ${req.url}`);
+  logger.info(`Request body, ${req.body}`);
+  next();
+});
 
 mongoose
   .connect(process.env.MONGODB_URL)
