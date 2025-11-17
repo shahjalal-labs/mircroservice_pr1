1e006cb43e94c9d6e9ab5a5970f5790bc48876da
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-17 21:47:15 +0600

    refactor: Updated identity-service/combined.log, src/server.js

--- identity-service/src/server.js
+++ identity-service/src/server.js
@@ -8,9 +8,14 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const logger = require("./utils/logger");
 
+const Redis = require("ioredis");
+const { RateLimiterRedis } = require("rate-limiter-flexible");
+
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+const redisClient = new Redis(process.env.REDIS_URL);
 
 //p: n! Secures the app by setting various HTTP headers
 //p: n! Helps prevent common attacks like XSS, clickjacking, and MIME sniffing
@@ -22,6 +27,26 @@ app.use((req, res, next) => {
   logger.info(`Received ${req.method} request to ${req.url}`);
   logger.info(`Request body, ${req.body}`);
   next();
+});
+
+const rateLimiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: "middleware",
+  points: 10,
+  duration: 1,
+});
+
+app.use((req, res, next) => {
+  rateLimiter
+    .consume(req.ip)
+    .then(() => next())
+    .catch(() => {
+      logger.warn(`Rate limit exceeded for IP: ${req.qip}`);
+      res.status(429).json({
+        success: false,
+        message: "Too many requests",
+      });
+    });
 });
 
 mongoose
