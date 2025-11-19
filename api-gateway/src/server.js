6d556d3094bd77d929fa12f038576d69e9d058a9
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-19 21:33:49 +0600

    feat: Added middleware/errorHandler.js | refactor: Updated api-gateway/package.json, src/server.js, utils/logger.js

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -1,2 +1,14 @@
 //
-console.log(`Hello World!`);
+require("dotenv").config();
+
+const express = require("express");
+
+const cors = require("cors");
+
+const Redis = require("ioredis");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const redisClient = new Redis(process.env.redis_url);
