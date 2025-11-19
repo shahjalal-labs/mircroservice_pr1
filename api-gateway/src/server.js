e6f865df80059749d76dee70b6a4ab2ca917aac5
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-19 21:40:49 +0600

    feat: Added dist/index.cjs, dist/index.d.cts, dist/index.d.mts, dist/index.d.ts, dist/index.mjs, express-rate-limit/license.md, express-rate-limit/package.json, express-rate-limit/readme.md, express-rate-limit/tsconfig.json, ip-address/LICENSE, ip-address/README.md, dist/address-error.d.ts, dist/address-error.d.ts.map, dist/address-error.js, dist/address-error.js.map, dist/common.d.ts, dist/common.d.ts.map, dist/common.js, dist/common.js.map, dist/ip-address.d.ts, dist/ip-address.d.ts.map, dist/ip-address.js, dist/ip-address.js.map, dist/ipv4.d.ts, dist/ipv4.d.ts.map, dist/ipv4.js, dist/ipv4.js.map, dist/ipv6.d.ts, dist/ipv6.d.ts.map, dist/ipv6.js, dist/ipv6.js.map, v4/constants.d.ts, v4/constants.d.ts.map, v4/constants.js, v4/constants.js.map, v6/constants.d.ts, v6/constants.d.ts.map, v6/constants.js, v6/constants.js.map, v6/helpers.d.ts, v6/helpers.d.ts.map, v6/helpers.js, v6/helpers.js.map, v6/regular-expressions.d.ts, v6/regular-expressions.d.ts.map, v6/regular-expressions.js, v6/regular-expressions.js.map, ip-address/package.json, src/address-error.ts, src/common.ts, src/ip-address.ts, src/ipv4.ts, src/ipv6.ts, v4/constants.ts, v6/constants.ts, v6/helpers.ts, v6/regular-expressions.ts, docs/cli_commands.md | refactor: Updated api-gateway/package.json, src/server.js

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -3,12 +3,43 @@ require("dotenv").config();
 
 const express = require("express");
 
+const helmet = require("helmet");
+
+const { rateLimit } = require("express-rate-limit");
+
 const cors = require("cors");
 
+const { RedisStore } = require("rate-limit-redis");
 const Redis = require("ioredis");
+const logger = require("./utils/logger");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const redisClient = new Redis(process.env.redis_url);
+const redisClient = new Redis(process.env.REDIS_URL);
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+// rate limiting
+
+const rateLimitOptions = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res) => {
+    logger.warn(`Sensitive endpoint rate limit exceeded for IP: ${req.ip}`);
+    res.status(429).json({
+      success: false,
+      message: "Too many requests. Try again after " + 15 * 60 * 1000,
+    });
+  },
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
+});
+
+app.use(rateLimitOptions);
