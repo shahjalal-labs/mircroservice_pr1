5a2f53638345b7ab71c7febc23cda46681d6c668
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-20 19:55:21 +0600

    refactor: Updated controllers/post-controller.js, utils/validation.js

--- post-service/src/controllers/post-controller.js
+++ post-service/src/controllers/post-controller.js
@@ -1,8 +1,25 @@
 const logger = require("../utils/logger");
+const { validateCreatePost } = require("../utils/validation");
 
 const createPost = async (req, res) => {
-  logger.inf("Create post endpoint hit");
+  logger.info("Create post endpoint hit");
 
   try {
-  } catch (e) {}
+    const { error } = validateCreatePost(req.body);
+    if (error) {
+      logger.warn("Validation error", error.details[0].message);
+      return res.status(400).json({
+        success: false,
+        message: error.details[0].message,
+      });
+    }
+
+    const { content, mediaIds } = req.body;
+  } catch (e) {
+    logger.error("Error creating post", error);
+    res.status(500).json({
+      success: false,
+      message: "Error creating post",
+    });
+  }
 };
--- post-service/src/utils/validation.js
+++ post-service/src/utils/validation.js
@@ -1,21 +1,13 @@
 //
 const Joi = require("joi");
 
-const createPost = (data) => {
+const validateCreatePost = (data) => {
   const schema = Joi.object({
     content: Joi.string().min(10),
-    mediaIds: Joi.array(Joi.string()),
+    mediaIds: Joi.array(),
   });
 
   return schema.validate(data);
 };
 
-const validatelogin = (data) => {
-  const schema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
-  });
-
-  return schema.validate(data);
-};
-module.exports = { validateRegistration: createPost, validatelogin };
+module.exports = { validateCreatePost };
