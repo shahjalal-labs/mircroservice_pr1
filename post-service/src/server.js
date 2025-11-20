e4dd45f62129dfa0560aea30203518dbfdd4ef44
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-20 20:30:21 +0600

    feat: Added commands/LICENSE, commands/README.md, built/commands.json, built/index.d.ts, built/index.js, commands/package.json, cluster-key-slot/.eslintrc, cluster-key-slot/LICENSE, cluster-key-slot/README.md, cluster-key-slot/index.d.ts, lib/index.js, cluster-key-slot/package.json, denque/CHANGELOG.md, denque/LICENSE, denque/README.md, denque/index.d.ts, denque/index.js, denque/package.json, dotenv/CHANGELOG.md, dotenv/LICENSE, dotenv/README-es.md, dotenv/README.md, dotenv/SECURITY.md, dotenv/config.d.ts, dotenv/config.js, lib/cli-options.js, lib/env-options.js, lib/main.d.ts, lib/main.js, dotenv/package.json, ioredis/LICENSE, ioredis/README.md, built/Command.d.ts, built/Command.js, built/DataHandler.d.ts, built/DataHandler.js, built/Pipeline.d.ts, built/Pipeline.js, built/Redis.d.ts, built/Redis.js, built/ScanStream.d.ts, built/ScanStream.js, built/Script.d.ts, built/Script.js, built/SubscriptionSet.d.ts, built/SubscriptionSet.js, built/autoPipelining.d.ts, built/autoPipelining.js, cluster/ClusterOptions.d.ts, cluster/ClusterOptions.js, cluster/ClusterSubscriber.d.ts, cluster/ClusterSubscriber.js, cluster/ClusterSubscriberGroup.d.ts, cluster/ClusterSubscriberGroup.js, cluster/ConnectionPool.d.ts, cluster/ConnectionPool.js, cluster/DelayQueue.d.ts, cluster/DelayQueue.js, cluster/index.d.ts, cluster/index.js, cluster/util.d.ts, cluster/util.js, connectors/AbstractConnector.d.ts, connectors/AbstractConnector.js, connectors/ConnectorConstructor.d.ts, connectors/ConnectorConstructor.js, SentinelConnector/FailoverDetector.d.ts, SentinelConnector/FailoverDetector.js, SentinelConnector/SentinelIterator.d.ts, SentinelConnector/SentinelIterator.js, SentinelConnector/index.d.ts, SentinelConnector/index.js, SentinelConnector/types.d.ts, SentinelConnector/types.js, connectors/StandaloneConnector.d.ts, connectors/StandaloneConnector.js, connectors/index.d.ts, connectors/index.js, constants/TLSProfiles.d.ts, constants/TLSProfiles.js, errors/ClusterAllFailedError.d.ts, errors/ClusterAllFailedError.js, errors/MaxRetriesPerRequestError.d.ts, errors/MaxRetriesPerRequestError.js, errors/index.d.ts, errors/index.js, built/index.d.ts, built/index.js, redis/RedisOptions.d.ts, redis/RedisOptions.js, redis/event_handler.d.ts, redis/event_handler.js, built/transaction.d.ts, built/transaction.js, built/types.d.ts, built/types.js, utils/Commander.d.ts, utils/Commander.js, utils/RedisCommander.d.ts, utils/RedisCommander.js, utils/applyMixin.d.ts, utils/applyMixin.js, utils/debug.d.ts, utils/debug.js, utils/index.d.ts, utils/index.js, utils/lodash.d.ts, utils/lodash.js, ioredis/package.json, lodash.defaults/LICENSE, lodash.defaults/README.md, lodash.defaults/index.js, lodash.defaults/package.json, lodash.isarguments/LICENSE, lodash.isarguments/README.md, lodash.isarguments/index.js, lodash.isarguments/package.json, redis-errors/.npmignore, redis-errors/LICENSE, redis-errors/README.md, redis-errors/index.js, lib/modern.js, lib/old.js, redis-errors/package.json, redis-parser/.npmignore, redis-parser/LICENSE, redis-parser/README.md, redis-parser/changelog.md, redis-parser/index.js, lib/parser.js, redis-parser/package.json, standard-as-callback/LICENSE, standard-as-callback/README.md, built/index.d.ts, built/index.js, built/types.d.ts, built/types.js, built/utils.d.ts, built/utils.js, standard-as-callback/package.json | refactor: Updated post-service/package.json, src/server.js

---
+++ post-service/src/server.js
@@ -0,0 +1,31 @@
+//
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const Redis = require("ioredis");
+const cors = require("cors");
+const helmet = require("helmet");
+const logger = require("./utils/logger");
+
+const app = express();
+
+const PORT = process.env.PORT || 3002;
+
+//connect to mongodb
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => logger.info("Connected to mongodb"))
+  .catch((e) => logger.error("Mongo connection error", e));
+
+const redisClient = new Redis(process.env.REDIS_URL);
+
+//middleware
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  logger.info(`Received ${req.method} request to ${req.url}`);
+  logger.info(`Request body, ${req.body}`);
+  next();
+});
