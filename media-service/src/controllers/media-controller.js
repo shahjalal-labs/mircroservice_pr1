444bf1ae781dbdecd7e2c3dfee713b7de884a6de
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-23 20:14:45 +0600

    refactor: Updated controllers/media-controller.js, routes/media-routes.js

--- media-service/src/controllers/media-controller.js
+++ media-service/src/controllers/media-controller.js
@@ -1,1 +1,31 @@
+//
+const { uploadMediaToCloudinary } = require("../utils/cloudinary");
+const logger = require("../utils/logger");
 
+const uploadMedia = async (req, res, next) => {
+  logger.info("Starting media upload");
+  if (!req.file) {
+    logger.error("No file found. Please add a file and try again!");
+    return res.status(400).json({
+      success: false,
+      message: "No file found. Please add a file and try again!",
+    });
+  }
+
+  const { originalname, mimetype, buffer } = req.file;
+
+  const userId = req.user.userId;
+  logger.info(`File details: name=${originalname}, type=${mimetype}`);
+  logger.info("Uploading to cloudinary starting...");
+  const cloudinaryUploadResult = await uploadMediaToCloudinary(req.file);
+  logger.info(
+    `Cloudinary upload successfully. Public Id: - ${cloudinaryUploadResult.public_id}`,
+  );
+
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { uploadMedia };
--- media-service/src/routes/media-routes.js
+++ media-service/src/routes/media-routes.js
@@ -1,23 +1,10 @@
 const express = require("express");
-const { authenticateRequest } = require("../middleware/authMiddleware");
-const {
-  createPost,
-  getAllPosts,
-  getPost,
-  deletePost,
-} = require("../controllers/post-controller");
-
 const router = express.Router();
 
 // middleware -> this will tell if the user is an auth user or not
 
 router.use(authenticateRequest);
 
-router.post("/create-post", createPost);
-router.get("/", getAllPosts);
-
-router.get("/:postId", getPost);
-
-router.delete("/:id", deletePost);
+router.post("/create-post");
 
 module.exports = router;
