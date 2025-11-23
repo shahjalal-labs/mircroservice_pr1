c54997dec10ec7ed5efe78bd321dd448b1de0d70
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-23 19:15:42 +0600

    refactor: Updated utils/cloudinary.js, utils/logger.js

--- media-service/src/utils/cloudinary.js
+++ media-service/src/utils/cloudinary.js
@@ -1,3 +1,5 @@
+const logger = require("./logger");
+
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
@@ -5,3 +7,23 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+const uploadMediaToCloudinary = (file) => {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      {
+        resource_type: "auto",
+      },
+      (err, result) => {
+        if (err) {
+          logger.error("Error while uploading media to cloudinary", err);
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      },
+    );
+  });
+};
+
+module.exports = { uploadMediaToCloudinary };
