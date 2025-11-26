810159b660e041e2bff966e82063f55dc4da961b
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-26 21:01:54 +0600

    refactor: Updated api-gateway/combined.log, api-gateway/error.log, src/server.js

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -117,7 +117,7 @@ app.use(
     ...proxyOptions,
     proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
       proxyReqOpts.headers["x-user-id"] = srcReq.user.userId;
-      if (!srcReq.headers["content-type"]?.startsWith("multipart/form-data")) {
+      if (!srcReq.headers["content-type"].startsWith("multipart/form-data")) {
         proxyReqOpts.headers["Content-Type"] = "application/json";
       }
 
