8b9311520beced7f2f01e76df84a5dd4ee8ff6d4
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-27 20:42:01 +0600

    refactor: Updated search-service/package.json, src/server.js

--- search-service/src/server.js
+++ search-service/src/server.js
@@ -5,3 +5,20 @@ const mongoose = require("mongoose");
 const Redis = require("ioredis");
 const cors = require("cors");
 const helmet = require("helmet");
+
+const app = express();
+const port = process.env.PORT || 3004;
+
+// connect to mongodb
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to mongodb"))
+  .catch((e) => logger.error("Mongo connection error", e));
+
+const redisClient = new Redis(process.env.REDIS_URL);
+
+//middleware
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
