200f3737050e0767475bdd7e5d5ba85aa4ecf7aa
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-27 20:36:07 +0600

    feat: Added cors/CONTRIBUTING.md, cors/HISTORY.md, cors/LICENSE, cors/README.md, lib/index.js, cors/package.json, helmet/CHANGELOG.md, helmet/LICENSE, helmet/README.md, helmet/SECURITY.md, helmet/index.cjs, helmet/index.d.cts, helmet/index.d.mts, helmet/index.mjs, helmet/package.json, object-assign/index.js, object-assign/license, object-assign/package.json, object-assign/readme.md | refactor: Updated search-service/package.json, src/server.js

--- search-service/src/server.js
+++ search-service/src/server.js
@@ -3,3 +3,5 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const Redis = require("ioredis");
+const cors = require("cors");
+const helmet = require("helmet");
