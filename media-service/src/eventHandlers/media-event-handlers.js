e6f6731fabff0331a1661f7b503e8098b304ac9c
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-27 19:18:01 +0600

    refactor: Updated media-service/combined.log, media-service/error.log, eventHandlers/media-event-handlers.js

--- media-service/src/eventHandlers/media-event-handlers.js
+++ media-service/src/eventHandlers/media-event-handlers.js
@@ -1,4 +1,12 @@
-const handlePostDeleted = async (event) => {};
+//
+const handlePostDeleted = async (event) => {
+  const { postId, mediaIds } = event;
+
+  try {
+  } catch (error) {
+    logger.error(error, "Error occured while media deletion");
+  }
+};
 
 module.exports = {
   handlePostDeleted,
