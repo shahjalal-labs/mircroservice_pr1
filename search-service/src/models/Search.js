af9bcb022601cfd0827d89e173da22f86bb27468
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-27 20:54:15 +0600

    feat: Added models/Search.js

---
+++ search-service/src/models/Search.js
@@ -0,0 +1,31 @@
+//
+const mongoose = require("mongoose");
+const searchPostSchema = new mongoose.Schema(
+  {
+    postId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Search = mongoose.model("Search", searchPostSchema);
+
+module.exports = Search;
