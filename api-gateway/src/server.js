f19857c03e9e15d1e9165e66a7219691c5359c24
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-29 18:16:17 +0600

    refactor: Updated api-gateway/combined.log, src/server.js, identity-service/combined.log, media-service/combined.log, media-service/error.log, post-service/combined.log, post-service/error.log

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -133,7 +133,26 @@ app.use(
     parseReqBody: false,
   }),
 );
+app.use(
+  "/v1/posts",
+  validateToken,
+  proxy(process.env.POST_SERVICE_URL, {
+    ...proxyOptions,
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      proxyReqOpts.headers["Content-Type"] = "application/json";
+      proxyReqOpts.headers["x-user-id"] = srcReq.user.userId;
 
+      return proxyReqOpts;
+    },
+    userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+      logger.info(
+        `Response received from Post service: ${proxyRes.statusCode}`,
+      );
+
+      return proxyResData;
+    },
+  }),
+);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
